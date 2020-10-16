53d930b3653b1471858fc584e059b49622a3a8d6
zarron <zarron@student.ethz.ch>
2020-10-16 15:59:58 +0200

    added quicktest file

---
+++ quicktests.ml
@@ -0,0 +1,19 @@
+(* 
+
+(Simulator.assemble Gradedtests.helloworld).text_seg;;
+*)
+
+(*
+
+#load "_build/int64_overflow.cmo";;
+#load "_build/x86/x86.cmo";;
+#load "_build/simulator.cmo";;
+#load "gradedtests.ml";;
+
+
+*)
+open Gradedtests;;
+
+Gradedtests.helloworld_textseg ;;
+
+#quit;; 
