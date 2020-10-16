beb64debfb4d0fd4b2c6182979d10b615ef88e81
zarron <zarron@student.ethz.ch>
2020-10-16 16:21:55 +0200

    added student test

--- quicktests.ml
+++ quicktests.ml
@@ -10,10 +10,30 @@
 #load "_build/simulator.cmo";;
 #load "gradedtests.ml";;
 
-
-*)
 open Gradedtests;;
+*)
+#use "util/assert.ml";;
+#use "x86/x86.ml";;
+#use "gradedtests.ml";;
+#use "simulator.ml";;
 
-Gradedtests.helloworld_textseg ;;
+let helloworld = [ text "foo"
+                            [ Xorq, [~%Rax; ~%Rax]
+                            ; Movq, [~$100; ~%Rax]
+                            ; Retq, []
+                            ]
+                     ; text "main" 
+                            [ Xorq, [~%Rax; ~%Rax]
+                            ; Movq, [Ind1 (Lbl "baz"); ~%Rax]
+                            ; Retq, []
+                            ]
+                     ; data "baz" 
+                            [ Quad (Lit 99L)
+                            ; Asciz "Hello, world!"
+                            ]
+                     ]
+in
+
+assemble helloworld ;;
 
 #quit;; 
--- studenttests.ml
+++ studenttests.ml
@@ -1,13 +1,46 @@
 open Assert
-open X86
 open Simulator
+open X86
+open Asm
 
-(* You can use this file for additional test cases to help your *)
-(* implementation.                                              *)
+(* Test suite for asm.ml *)
 
+(* Do NOT modify this file -- we will overwrite it with our *)
+(* own version when we test your project.                   *)
+
+(* These tests will be used to grade your assignment *)
+
+(* Example Programs *)
+
+let helloworld = [ text "foo"
+                            [ Xorq, [~%Rax; ~%Rax]
+                            ; Movq, [~$100; ~%Rax]
+                            ; Retq, []
+                            ]
+                     ; text "main" 
+                            [ Xorq, [~%Rax; ~%Rax]
+                            ; Movq, [Ind1 (Lbl "baz"); ~%Rax]
+                            ; Retq, []
+                            ]
+                     ; data "baz" 
+                            [ Quad (Lit 99L)
+                            ; Asciz "Hello, world!"
+                            ]
+                     ]
+
+
+let helloworld_textseg =
+  [ InsB0 (Xorq, [Reg Rax; Reg Rax]);              InsFrag;InsFrag;InsFrag;InsFrag;InsFrag;InsFrag;InsFrag
+  ; InsB0 (Movq, [Imm (Lit 100L); Reg Rax]);       InsFrag;InsFrag;InsFrag;InsFrag;InsFrag;InsFrag;InsFrag
+  ; InsB0 (Retq, []);                              InsFrag;InsFrag;InsFrag;InsFrag;InsFrag;InsFrag;InsFrag
+  ; InsB0 (Xorq, [Reg Rax; Reg Rax]);              InsFrag;InsFrag;InsFrag;InsFrag;InsFrag;InsFrag;InsFrag
+  ; InsB0 (Movq, [Ind1 (Lit 0x400030L); Reg Rax]); InsFrag;InsFrag;InsFrag;InsFrag;InsFrag;InsFrag;InsFrag
+  ; InsB0 (Retq, []);                              InsFrag;InsFrag;InsFrag;InsFrag;InsFrag;InsFrag;InsFrag
+  ]
 
 let provided_tests : suite = [
-  Test ("Debug", [
+  Test ("Debug", [ 
+    ("assemble1", assert_eqf (fun () -> (assemble helloworld).text_seg) helloworld_textseg )
     ]);
 
 ] 
